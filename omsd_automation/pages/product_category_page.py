a71f151ee7a0585ed4694ce06007c44b6e01a2a3
Feroze basha S <bashaferoz66@gmail.com>
2025-09-23 23:13:18 +0530

    Add ProductCategoryPage class and update browser setting to Edge in config

---
+++ omsd_automation/pages/product_category_page.py
@@ -0,0 +1,20 @@
+﻿# product_category_page.py
+
+from playwright.sync_api import Page
+from omsd_automation.pages.base_page import BasePage
+
+class ProductCategoryPage(BasePage):
+    """Page class for Product Category interactions."""
+
+    def __init__(self, page: Page):
+        super().__init__(page)
+        self.page = page
+
+    def get_product_button(self, product_name: str):
+        """Returns the button locator for a given product's software list."""
+        return self.page.locator("#medical-product-box div").filter(has_text=f"{product_name} Software List").get_by_role("button")
+    def other_product_button(self, product_name: str):
+        return self.page.locator("#other-product-box div").filter(has_text=f"{product_name} Software List").get_by_role("button")
+    def get_back_button(self):
+        """Returns the back button locator."""
+        return self.page.get_by_role("button", name="Back")
